46096a1cff5279f27c696623e98f269f253453ba
Aditya Kumar Singh <adityasohitsingh@gmail.com>
2025-09-01 22:28:58 +0530

    url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("https://chatifyy-chat-app.onrender.com", {
+            const socket = io("https://chatify-chat-app-bdla.onrender.com", {
                 query: {
                     userId: authUser._id
                 },
